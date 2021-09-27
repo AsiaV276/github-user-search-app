e123c1c857d1f7bfaf0460c0a38ba4bf5bee089c
Asia Valentine <asiavalentine22@gmail.com>
2021-09-27 19:26:26 -0400

    fixed multiplying links

--- public/githubAPI.js
+++ public/githubAPI.js
@@ -13,8 +13,8 @@ var websiteLink = document.getElementById('website-link');
 var twitter = document.getElementById('twitter');
 var company = document.getElementById('company');
 var bio = document.getElementById('profile-bio');
-var usernameLinkH3 = document.getElementById('username-link');
-var websiteLinkSpan = document.getElementById('website-link');
+var usernameLink = document.getElementById('username-link');
+var websiteLink = document.getElementById('website-link');
 
 var userInput = document.getElementById('input-box').value = 'asiav276';
 
@@ -27,11 +27,9 @@ const getUserData = async () => {
         if(response != null) {
             //box header elements
             username.innerHTML = response.login
-            var usernameLink = document.createElement('a')
-                usernameLink.appendChild(document.createTextNode('@'+response.login.toLowerCase()))
-                usernameLink.href = response.html_url
-                usernameLink.target = '_blank'
-                usernameLinkH3.appendChild(usernameLink)
+            usernameLink.innerText = '@'+response.login.toLowerCase()
+            usernameLink.href = response.html_url
+            usernameLink.target = '_blank'
             profilePic.src = response.avatar_url
             joined.innerHTML = new Date(Date.parse(response.created_at)).toDateString()
             response.bio != null ? bio.innerText = response.bio : bio.innerText = "This profile has no bio."
@@ -45,17 +43,16 @@ const getUserData = async () => {
             userLocation.innerText = response.location
             website.innerText = response.blog
             company.innerText = response.company
-            console.log(response.blog);
+    
             if (response.blog != null) {
-                website.style.display = "none"
-                var websiteLink = document.createElement('a')
-                websiteLink.appendChild(document.createTextNode(response.blog))
+                website.style.display = 'none'
+                websiteLink.innerText = response.blog
                 websiteLink.href = 'https://' + response.blog
                 websiteLink.target = '_blank'
-                websiteLinkSpan.appendChild(websiteLink)
             }
             else {
                 website.innerText = 'Not Available'
+                websiteLink.innerText = ''
             }
     
             response.twitter_username != null ? twitter.innerText = response.twitter_username : twitter.innerHTML = "Not Available"
@@ -69,15 +66,29 @@ const getUserData = async () => {
 
 getUserData()
 
-/* Toggling dark or light theme?
-var mql = window.matchMedia('prefers-color-scheme: dark')
+// Toggling dark or light theme
+var darkBtn = document.getElementById('dark')
+var lightBtn = document.getElementById('light')
+var mql = window.matchMedia('(prefers-color-scheme: dark)')
+//var isDarkPref
 
-const screenTest =(e) => {
-    if(e.matches) {
+function screenTest(e) {
+    if (e.matches) {
+      /* computer's prefered theme is dark */
+      //console.log('the computers prefered theme is dark');
+      //isDarkPref = true
 
+    } else {
+      /* computer's prefered theme is light */
+      //console.log('the computers prefered theme is light');
+      //isDarkPref = false
     }
-}
+  }
+  
+  mql.addEventListener('change', screenTest);
+
+
 const toggleTheme = () => {
-    //if()
+    //console.log('dark theme is ' + isDarkPref);
 }
-*/
+//toggleTheme()
